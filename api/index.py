8a172bf23c2a817814dd563f9ca2a973c3261fee
agonzalezz <alegonzalez@dc.uba.ar>
2026-01-12 17:51:45 -0300

    change load dotenv

--- api/index.py
+++ api/index.py
@@ -1,3 +1,8 @@
-from dotenv import load_dotenv
-load_dotenv()
+import os
+
+# SOLO cargar dotenv en local
+if os.getenv("VERCEL") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
 from adapters.http.vercel.main import app
